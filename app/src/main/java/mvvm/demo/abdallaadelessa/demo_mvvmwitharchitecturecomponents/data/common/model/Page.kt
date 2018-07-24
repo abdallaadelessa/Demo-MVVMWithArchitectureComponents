3c442581ba066a7ab1e012c97a317b5b30cc4560
Abdullah Adel Essa <aa@next-munich.com>
2018-07-24 11:30:04 +0200

    Clean code

--- app/src/main/java/mvvm/demo/abdallaadelessa/demo_mvvmwitharchitecturecomponents/data/common/model/Page.kt
+++
@@ -1,5 +0,0 @@
-package mvvm.demo.abdallaadelessa.demo_mvvmwitharchitecturecomponents.data.common.model
-
-class Page<E>(val data : List<E>?) {
-  val isEmpty  get()= data== null || data.isEmpty();
-}
